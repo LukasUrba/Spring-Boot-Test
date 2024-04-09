aebdaa193dd5f8b987c1a6a93ef2b9e69549f7ae
Lukas Urba <lukas.urba@gmail.com>
2024-04-09 18:31:26 +0100

    Added log

--- src/main/java/lukasurba/test/springBootTest/SpringBootTestApplication.java
+++ src/main/java/lukasurba/test/springBootTest/SpringBootTestApplication.java
@@ -1,5 +1,8 @@
 package lukasurba.test.springBootTest;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,10 +10,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SpringBootTestApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootTestApplication.class);
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringBootTestApplication.class, args);
-		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
-		System.out.println(welcomeMessage);
+		log.info("Something New!");
+//		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+//		System.out.println(welcomeMessage);
 
 	}
 
--- src/main/java/lukasurba/test/springBootTest/WelcomeMessage.java
+++ src/main/java/lukasurba/test/springBootTest/WelcomeMessage.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 //Any Components should be within the same package as main to deal with Springs dependency injection & version control
 public class WelcomeMessage {
     public String getWelcomeMessage() {
-        return "Welcome to the Appliaction!";
+        return "Welcome to the Application!";
     }
 
     @Override
