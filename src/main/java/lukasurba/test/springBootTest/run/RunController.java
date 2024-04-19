56405a2bb70b3e7949c08d8b1f41b37a58e00105
Lukas Urba <lukas.urba@gmail.com>
2024-04-19 15:10:19 +0100

    Commented out code to be moved later and added Jdbc client

--- src/main/java/lukasurba/test/springBootTest/run/RunController.java
+++ src/main/java/lukasurba/test/springBootTest/run/RunController.java
@@ -16,40 +16,39 @@ public class RunController {
     private final RunRepo runRepo;
 
 //    Autowired here is implicit
-    @Autowired
     public RunController(RunRepo runRepo) {
         this.runRepo = runRepo;
     }
 
-    @GetMapping("")
-    List<Run> findAll() {
-        return runRepo.findAll();
-    }
-
-    @GetMapping("/{id}")
-    Run findByID(@PathVariable Integer id) {
-        Optional<Run> run = runRepo.findByID(id);
-        if (run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run Not Found");
-        }
-        return run.get();
-    }
-
-    @PostMapping("")
-    @ResponseStatus(HttpStatus.CREATED)
-    void create(@Valid @RequestBody Run run) {
-        runRepo.createRun(run);
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
-    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
-        runRepo.updateRun(run,id);
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void delete(@PathVariable Integer id) {
-        runRepo.deleteRun(id);
-    }
+//    @GetMapping("")
+//    List<Run> findAll() {
+//        return runRepo.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    Run findByID(@PathVariable Integer id) {
+//        Optional<Run> run = runRepo.findByID(id);
+//        if (run.isEmpty()){
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run Not Found");
+//        }
+//        return run.get();
+//    }
+//
+//    @PostMapping("")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    void create(@Valid @RequestBody Run run) {
+//        runRepo.createRun(run);
+//    }
+//
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @PutMapping("/{id}")
+//    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+//        runRepo.updateRun(run,id);
+//    }
+//
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/{id}")
+//    void delete(@PathVariable Integer id) {
+//        runRepo.deleteRun(id);
+//    }
 }
--- src/main/java/lukasurba/test/springBootTest/run/RunRepo.java
+++ src/main/java/lukasurba/test/springBootTest/run/RunRepo.java
@@ -1,6 +1,7 @@
 package lukasurba.test.springBootTest.run;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,32 +12,38 @@ import java.util.Optional;
 
 @Repository
 public class RunRepo {
-    private final List<Run> allRuns = new ArrayList<>();
 
-    List<Run> findAll() {
-        return allRuns;
-    }
+    private final JdbcClient jdbcClient;
 
-    Optional<Run> findByID(Integer id) {
-        return allRuns.stream().filter(run -> Objects.equals(run.id(), id)).findFirst();
+    public RunRepo(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
+//    private final List<Run> allRuns = new ArrayList<>();
 
-    void createRun(Run run) {
-        allRuns.add(run);
-    }
-
-    @PostConstruct
-    private void init() {
-        allRuns.add(new Run(1,"My Run", LocalDateTime.now().minusMinutes(30),LocalDateTime.now(),4, Location.OUTDOOR));
-        allRuns.add(new Run(2,"My Run 2", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30),3, Location.OUTDOOR));
-    }
-
-    void updateRun(Run run, Integer id) {
-        Optional<Run> existingRun = findByID(id);
-        existingRun.ifPresent(value -> allRuns.set(allRuns.indexOf(value), run));
-    }
-
-    void deleteRun(Integer id) {
-        allRuns.removeIf(run -> run.id().equals(id));
-    }
+//    List<Run> findAll() {
+//        return allRuns;
+//    }
+//
+//    Optional<Run> findByID(Integer id) {
+//        return allRuns.stream().filter(run -> Objects.equals(run.id(), id)).findFirst();
+//    }
+//
+//    void createRun(Run run) {
+//        allRuns.add(run);
+//    }
+//
+//    @PostConstruct
+//    private void init() {
+//        allRuns.add(new Run(1,"My Run", LocalDateTime.now().minusMinutes(30),LocalDateTime.now(),4, Location.OUTDOOR));
+//        allRuns.add(new Run(2,"My Run 2", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30),3, Location.OUTDOOR));
+//    }
+//
+//    void updateRun(Run run, Integer id) {
+//        Optional<Run> existingRun = findByID(id);
+//        existingRun.ifPresent(value -> allRuns.set(allRuns.indexOf(value), run));
+//    }
+//
+//    void deleteRun(Integer id) {
+//        allRuns.removeIf(run -> run.id().equals(id));
+//    }
 }
