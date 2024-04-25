5631896d3d6f9c251f64d5d481d0922960338b56
Lukas Urba <lukas.urba@gmail.com>
2024-04-25 17:33:57 +0100

    Added a bunch of methods that use jdbcClient

--- src/main/java/lukasurba/test/springBootTest/run/RunRepo.java
+++ src/main/java/lukasurba/test/springBootTest/run/RunRepo.java
@@ -3,6 +3,7 @@ package lukasurba.test.springBootTest.run;
 import jakarta.annotation.PostConstruct;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -19,6 +20,12 @@ public class RunRepo {
         this.jdbcClient = jdbcClient;
     }
 //    private final List<Run> allRuns = new ArrayList<>();
+
+//    @PostConstruct
+//    private void init() {
+//        allRuns.add(new Run(1,"My Run", LocalDateTime.now().minusMinutes(30),LocalDateTime.now(),4, Location.OUTDOOR));
+//        allRuns.add(new Run(2,"My Run 2", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30),3, Location.OUTDOOR));
+//    }
 
     List<Run> findAll() {
         return jdbcClient.sql("select * from run")
@@ -37,20 +44,37 @@ public class RunRepo {
         var updated = jdbcClient.sql("INSERT INTO Run(id,title,started_on,finished_on, miles, location) values(?,?,?,?,?,?")
                 .params(List.of(run.id(),run.title(),run.startedOn(),run.finishedOn(),run.miles(),run.location().toString()))
                 .update();
+        Assert.state(updated==1,"Failed to create run " + run.title());
     }
 
-//    @PostConstruct
-//    private void init() {
-//        allRuns.add(new Run(1,"My Run", LocalDateTime.now().minusMinutes(30),LocalDateTime.now(),4, Location.OUTDOOR));
-//        allRuns.add(new Run(2,"My Run 2", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30),3, Location.OUTDOOR));
-//    }
-//
-//    void updateRun(Run run, Integer id) {
-//        Optional<Run> existingRun = findByID(id);
-//        existingRun.ifPresent(value -> allRuns.set(allRuns.indexOf(value), run));
-//    }
-//
-//    void deleteRun(Integer id) {
-//        allRuns.removeIf(run -> run.id().equals(id));
-//    }
+    void updateRun(Run run, Integer id) {
+        var updated = jdbcClient.sql("UPDATE Run SET title = ?, started_on = ?, finished_on = ?, miles = ?, location = ? WHERE id = ?")
+                .params((List.of(run.title(),run.startedOn(),run.finishedOn(),run.miles(),run.location().toString(),id)))
+                .update();
+        Assert.state(updated==1,"Failed to update run "+ run.title());
+    }
+
+    void deleteRun(Integer id) {
+        var updated = jdbcClient.sql("DELETE FROM Run WHERE id= :id")
+                .params("id", id)
+                .update();
+        Assert.state(updated==1,"Failed to delete run "+ id);
+    }
+
+    public int count() {
+        return jdbcClient.sql("SELECT * FROM run")
+                .query()
+                .listOfRows()
+                .size();
+    }
+
+    public void savelAllRuns(List<Run> runs){
+        runs.stream().forEach(this::createRun);
+    }
+    public List<Run> findByLocation(String location) {
+        return jdbcClient.sql("SELECT * FROM run WHERE location = :location")
+                .params("location",location)
+                .query(Run.class)
+                .list();
+    }
 }
